f6c5c8525ac67e8b80d98dfb139991b130bd2e29
sarthak <sarthakagrawal8@gmail.com>
2020-07-29 17:47:26 +0530

    update utilities

--- Round1/test.py
+++ Round1/test.py
@@ -3,7 +3,7 @@ import utility
 from time import time
 
 st = time()
-for fl in utility.get_files('static_samples/benign'):
+for fl in utility.get_all('static_samples'):
     string_analysis.get_frequency_map(fl)
 end = time()
 print ('String feature extraction complete in ' + str(end-st) + ' seconds')
--- Round1/utility/__init__.py
+++ Round1/utility/__init__.py
@@ -16,3 +16,19 @@ def get_files(directory):
     for f in os.scandir(directory):
         if f.is_dir():
             yield f.path
+
+def get_all(root):
+    for cl in classes:
+        yield from get_files(os.path.join(root, cl))
+def get_benigns(root):
+    return get_files(os.path.join(root, classes[0]))
+def get_trojans(root):
+    return get_files(os.path.join(root, 'malware/trojan'))
+def get_trojandownloaders(root):
+    return get_files(os.path.join(root, 'malware/trojandownloader'))
+def get_trojandroppers(root):
+    return get_files(os.path.join(root, 'malware/trojandropper'))
+def get_viruses(root):
+    return get_files(os.path.join(root, 'malware/virus'))
+def get_worms(root):
+    return get_files(os.path.join(root, 'malware/worm'))
