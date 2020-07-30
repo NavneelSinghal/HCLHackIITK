adbe26097800b3f00696a37945b00dc166d14cd1
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-07-31 01:11:26 +0530

    dumping in ml.py

--- Round1/ml.py
+++ Round1/ml.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # the full machine learning pipeline after feature extraction
 #
 # author: Rishabh Ranjan
@@ -15,19 +17,26 @@ from sklearn.feature_extraction import FeatureHasher
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 from time import time
+from pickle import dump, load
+from sys import argv
 
 feature_dict_func = get_feature_dict
-num_features = 1000
+num_features = 20000
 root = 'Static_Analysis_Data'
 train_frac = 0.75
+use_frac = 0.5 # fraction of files used to train+test
+root_cute = 'static'
+time_stamp = str(int(time()))
 
 def get_filenames_arr(train_frac):
     benigns = get_benigns(root)
     malwares = get_malwares(root)
-    num_train_benigns = int(train_frac * len(benigns))
-    num_train_malwares = int(train_frac * len(malwares))
+    num_used_benigns = int(use_frac * len(benigns))
+    num_used_malwares = int(use_frac * len(malwares))
+    num_train_benigns = int(train_frac * num_used_benigns)
+    num_train_malwares = int(train_frac * num_used_malwares)
     train = [benigns[:num_train_benigns], malwares[:num_train_malwares]]
-    test = [benigns[num_train_benigns:], malwares[num_train_malwares:]]
+    test = [benigns[num_train_benigns:num_used_benigns], malwares[num_train_malwares:num_used_malwares]]
     return train, test
 
 def get_D_y_aux(filenames, category):
@@ -54,7 +63,7 @@ def get_D_y(filenames_arr):
         y += ty
     return D, y
 
-def get_trained_estimator(D, y):
+def get_trained_classifier(D, y):
     pipe = make_pipeline(
         FeatureHasher(n_features=num_features),
         StandardScaler(with_mean=False),
@@ -64,21 +73,29 @@ def get_trained_estimator(D, y):
     pipe.fit(D, y)
     return pipe
 
-def print_metrics(y_true, y_pred):
-    print('accuracy:', accuracy_score(y_true, y_pred))
-    print('f1 (micro):', f1_score(y_true, y_pred, average='micro'))
-    print('recall (micro):', recall_score(y_true, y_pred, average='micro'))
-    print('precision (micro):', precision_score(y_true, y_pred, average='micro'))
-    print('f1 (macro):', f1_score(y_true, y_pred, average='macro'))
-    print('recall (macro):', recall_score(y_true, y_pred, average='macro'))
-    print('precision (macro):', precision_score(y_true, y_pred, average='macro'))
+def get_metrics(y_true, y_pred):
+    ret = ''
+    ret += 'accuracy:' + str(accuracy_score(y_true, y_pred))
+    ret += '\n'
+    ret += 'f1 (micro):' + str(f1_score(y_true, y_pred, average='micro'))
+    ret += '\n'
+    ret += 'recall (micro):' + str(recall_score(y_true, y_pred, average='micro'))
+    ret += '\n'
+    ret += 'precision (micro):' + str(precision_score(y_true, y_pred, average='micro'))
+    ret += '\n'
+    ret += 'f1 (macro):' + str(f1_score(y_true, y_pred, average='macro'))
+    ret += '\n'
+    ret += 'recall (macro):' + str(recall_score(y_true, y_pred, average='macro'))
+    ret += '\n'
+    ret += 'precision (macro):' + str(precision_score(y_true, y_pred, average='macro'))
+    ret += '\n'
+    return ret
 
-def main():
+def extract():
     print('extracting filenames...')
     train_filenames_arr, test_filenames_arr = get_filenames_arr(train_frac)
     print('train filenames: benigns =', len(train_filenames_arr[0]), '\tmalwares =', len(train_filenames_arr[1]))
     print('test filenames:  benigns =', len(test_filenames_arr[0]), '\tmalwares =', len(test_filenames_arr[1]))
-    print('---')
     print('extracting train features...')
     start = time()
     D_train, y_train = get_D_y(train_filenames_arr)
@@ -86,27 +103,55 @@ def main():
     print('time for extracting train features:', finish-start, 's')
     print('time for extracting train features per train file:', (finish-start)/len(y_train), 's')
     print('number of features:', len(D_train[0]))
-    print('---')
-    print('training estimator...')
-    start = time()
-    clf = get_trained_estimator(D_train, y_train)
-    finish = time()
-    print('time for training estimator:', finish-start, 's')
-    print('time for training estimator per train file:', (finish-start)/len(y_train), 's')
-    print('---')
     print('extracting test features...')
     start = time()
     D_test, y_test = get_D_y(test_filenames_arr)
     finish = time()
     print('time for extracting test features:', finish-start, 's')
     print('time for extracting test features per train file:', (finish-start)/len(y_train), 's')
+    f = '_Dy_' + root_cute + '_' + str(use_frac).split('.')[-1] + '_' + str(train_frac).split('.')[-1] + '_' + time_stamp
+    dump((D_train, y_train, D_test, y_test), open(f, 'wb'))
+    print('(D_train, y_train, D_test, y_test) dumped in:', f)
     print('---')
+    return D_train, y_train, D_test, y_test
+
+def train(D_train, y_train):
+    print('training classifier...')
+    start = time()
+    clf = get_trained_classifier(D_train, y_train)
+    finish = time()
+    print('time for training classifier:', finish-start, 's')
+    print('time for training classifier per train file:', (finish-start)/len(y_train), 's')
+    f = '_clf_' + root_cute + '_' + time_stamp
+    dump(clf, open(f, 'wb'))
+    print('classifier dumped in:', f)
+    print('---')
+    return clf
+
+def test(clf, D_test, y_test):
     print('testing...')
+    start = time()
     y_pred = clf.predict(D_test)
+    finish = time()
     print('time for testing:', finish-start, 's')
     print('time for testing per test file:', (finish-start)/len(y_test), 's')
+    metrics = get_metrics(y_test, y_pred)
+    f = '_metrics_' + root_cute + '_' + time_stamp
+    print(metrics, file=open(f, 'w'))
+    print('metrics logged in:', f)
     print('---')
-    print_metrics(y_test, y_pred)
+    return metrics
+
+def main():
+    if len(argv) < 2 or argv[1] == '-':
+        D_train, y_train, D_test, y_test = extract()
+    else:
+        D_train, y_train, D_test, y_test = load(open(argv[1], 'rb'))
+    if len(argv) < 3 or argv[2] == '-':
+        clf = train(D_train, y_train)
+    else:
+        clf = load(open(argv[2], 'rb'))
+    test(clf, D_test, y_test)
 
 if __name__ == '__main__':
     main()
