0165b693f185c04254dcf17812cbe7d075657d21
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-07-31 16:06:13 +0530

    modified ml.py

--- Round1/ml.py
+++ Round1/ml.py
@@ -6,12 +6,9 @@
 #
 # TODO: theoretically correct use of standard scaling
 
-from structure_analysis import *
-from utility import *
+import structure_analysis
+import dynamic_analysis
 
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
@@ -20,7 +17,7 @@ from time import time
 from pickle import dump, load
 from sys import argv
 
-feature_dict_func = get_feature_dict_no_indent
+feature_dict_func = structure_analysis.get_feature_dict_no_indent
 num_features = 10000
 root = 'Static_Analysis_Data'
 train_frac = 0.75
@@ -28,85 +25,109 @@ use_frac = 1.0 # fraction of files used to train+test
 root_cute = 'static'
 time_stamp = str(int(time()))
 
-def get_filenames_arr(train_frac):
-    benigns = get_benigns(root)
-    backdoors = get_backdoors(root)
-    trojans = get_trojans(root)
-    trojandownloaders = get_trojandownloaders(root)
-    trojandroppers = get_trojandroppers(root)
-    viruses = get_viruses(root)
-    worms = get_worms(root)
-    num_used_benigns = int(use_frac * len(benigns))
-    num_used_backdoors = int(use_frac * len(backdoors))
-    num_used_trojans = int(use_frac * len(trojans))
-    num_used_trojandownloaders = int(use_frac * len(trojandownloaders))
-    num_used_trojandroppers = int(use_frac * len(trojandroppers))
-    num_used_viruses = int(use_frac * len(viruses))
-    num_used_worms = int(use_frac * len(worms))
-    num_train_benigns = int(train_frac * num_used_benigns)
-    num_train_backdoors = int(train_frac * num_used_backdoors)
-    num_train_trojans = int(train_frac * num_used_trojans)
-    num_train_trojandownloaders = int(train_frac * num_used_trojandownloaders)
-    num_train_trojandroppers = int(train_frac * num_used_trojandroppers)
-    num_train_viruses = int(train_frac * num_used_viruses)
-    num_train_worms = int(train_frac * num_used_worms)
-    train = [
-        benigns[:num_train_benigns],
-        backdoors[:num_train_backdoors],
-        trojans[:num_train_trojans],
-        trojandownloaders[:num_train_trojandownloaders],
-        trojandroppers[:num_train_trojandroppers],
-        viruses[:num_train_viruses],
-        worms[:num_train_worms]
-    ]
-    test = [
-        benigns[num_train_benigns:num_used_benigns],
-        backdoors[num_train_backdoors:num_used_backdoors],
-        trojans[num_train_trojans:num_used_trojans],
-        trojandownloaders[num_train_trojandownloaders:num_used_trojandownloaders],
-        trojandroppers[num_train_trojandroppers:num_used_trojandroppers],
-        viruses[num_train_viruses:num_used_viruses],
-        worms[num_train_worms:num_used_worms]
-    ]
+import glob
+import numpy
+import os
 
-    return train, test
+root = '.'
+num_features = 5000
+dump_label = 'structure'
+glob_str = 'Structure_Info.txt'
+time_stamp = str(int(time()))
+#get_features = lambda path: ([0.], structure_analysis.get_feature_dict(os.path.split(path)[0]))
+get_features = dynamic_analysis.get_feature_vector
 
-def get_D_y_aux(filenames, category):
+class_dict = {
+    'benign':0,
+    'backdoor':1,
+    'trojan':2,
+    'trojandownloader':3,
+    'trojandropper':4,
+    'virus':5,
+    'worm':6
+}
+
+def get_paths(root, glob_str, use_frac = 1., train_frac = .7):
+    paths = glob.glob(os.path.join(root, '**', glob_str), recursive=True)
+    class_wise = []
+    for _ in range(len(class_dict)):
+        class_wise.append([])
+    for path in paths:
+        h, t = os.path.split(path)
+        hh, ht = os.path.split(h)
+        _, hht = os.path.split(hh)
+        if ht in class_dict:
+            class_id = class_dict[ht]
+        elif hht in class_dict:
+            class_id = class_dict[hht]
+        else:
+            class_id = None
+        if class_id is not None: 
+            class_wise[class_id].append(path)
+    class_wise_train = []
+    class_wise_test = []
+    for i in range(len(class_dict)):
+        num_use = int(use_frac * len(class_wise[i]))
+        num_train = int(train_frac * num_use)
+        class_wise_train.append(class_wise[i][:num_train])
+        class_wise_test.append(class_wise[i][num_train:num_use])
+    train_paths = []
+    test_paths = []
+    for i in range(len(class_dict)):
+        for path in class_wise_train[i]:
+            train_paths.append((path, i))
+        for path in class_wise_test[i]:
+            test_paths.append((path, i))
+    return train_paths, test_paths
+
+def get_X_y(paths, get_features):
+    L = []
     D = []
     y = []
-    ctr = 0
-    for f in filenames:
-        print('category:', category, 'file#:', ctr, end='\r')
+    for path, class_id in paths:
         try:
-            D.append(feature_dict_func(f))
-            y.append(category)
-            ctr += 1
+            l, d = get_features(path)
         except:
-            pass
-    print()
-    return D, y
+            continue
+        L.append(l)
+        D.append(d)
+        y.append(class_id)
+    fh = FeatureHasher(n_features=num_features)
+    H = fh.transform(D).toarray()
+    X = numpy.concatenate((H, L), axis=1)
+    return X, y
 
-def get_D_y(filenames_arr):
-    D = []
-    y = []
-    for c in range(len(filenames_arr)):
-        tD, ty = get_D_y_aux(filenames_arr[c], c)
-        D += tD
-        y += ty
-    return D, y
+def extract():
+    #train_paths, test_paths = get_paths('.', 'Structure_Info.txt', 1., .7)
+    train_paths, test_paths = get_paths('.', '*.json', 1., .7)
+    print('extracting features...')
+    start = time()
+    X_train, y_train = get_X_y(train_paths, get_features)
+    X_test, y_test = get_X_y(test_paths, get_features)
+    finish = time()
+    print('time for extracting features:', finish-start, 's')
+    print('time for extracting features per file:', (finish-start)/len(y_train), 's')
+    f = '_Xy_' + root_cute + '_' + str(use_frac).split('.')[-1] + '_' + str(train_frac).split('.')[-1] + '_' + time_stamp
+    dump((X_train, y_train, X_test, y_test), open(f, 'wb'))
+    print('(X_train, y_train, X_test, y_test) dumped in:', f)
+    print('---')
+    return X_train, y_train, X_test, y_test
 
-def get_trained_classifier(D, y):
-    pipe = make_pipeline(
-        FeatureHasher(n_features=num_features),
-        # StandardScaler(with_mean=False),
-        # VarianceThreshold(),
-        RandomForestClassifier(random_state=0)
-    )
-    pipe.fit(D, y)
-    return pipe
+def train(X, y):
+    print('training classifier...')
+    start = time()
+    clf = RandomForestClassifier(random_state=0)
+    clf.fit(X, y)
+    finish = time()
+    print('time for training classifier:', finish-start, 's')
+    print('time for training classifier per file:', (finish-start)/len(y), 's')
+    f = '_clf_' + root_cute + '_' + time_stamp
+    dump(clf, open(f, 'wb'))
+    print('classifier dumped in:', f)
+    print('---')
+    return clf
 
 def get_metrics(y_true, y_pred, binary=True):
-    ret = ''
     if binary:
         y_true = [(6-y)//6 for y in y_true]
         y_pred = [(6-y)//6 for y in y_pred]
@@ -114,6 +135,7 @@ def get_metrics(y_true, y_pred, binary=True):
     for i in range(len(y_true)):
         if y_true[i] != y_pred[i]:
             ctr += 1
+    ret = ''
     ret += 'mismatch:' + str(ctr)
     ret += '\n'
     ret += 'accuracy:' + str(accuracy_score(y_true, y_pred))
@@ -130,59 +152,21 @@ def get_metrics(y_true, y_pred, binary=True):
     ret += '\n'
     ret += 'precision (macro):' + str(precision_score(y_true, y_pred, average='macro'))
     ret += '\n'
-    return ret
-
-def extract():
-    print('extracting filenames...')
-    train_filenames_arr, test_filenames_arr = get_filenames_arr(train_frac)
-    #print('train filenames: benigns =', len(train_filenames_arr[0]), '\tmalwares =', len(train_filenames_arr[1]))
-    #print('test filenames:  benigns =', len(test_filenames_arr[0]), '\tmalwares =', len(test_filenames_arr[1]))
-    print('extracting train features...')
-    start = time()
-    D_train, y_train = get_D_y(train_filenames_arr)
-    finish = time()
-    print('time for extracting train features:', finish-start, 's')
-    print('time for extracting train features per train file:', (finish-start)/len(y_train), 's')
-    print('number of features:', len(D_train[0]))
-    print('extracting test features...')
-    start = time()
-    D_test, y_test = get_D_y(test_filenames_arr)
-    finish = time()
-    print('time for extracting test features:', finish-start, 's')
-    print('time for extracting test features per train file:', (finish-start)/len(y_train), 's')
-    f = '_Dy_' + root_cute + '_' + str(use_frac).split('.')[-1] + '_' + str(train_frac).split('.')[-1] + '_' + time_stamp
-    dump((D_train, y_train, D_test, y_test), open(f, 'wb'))
-    print('(D_train, y_train, D_test, y_test) dumped in:', f)
-    print('---')
-    return D_train, y_train, D_test, y_test
-
-def train(D_train, y_train):
-    print('training classifier...')
-    start = time()
-    clf = get_trained_classifier(D_train, y_train)
-    finish = time()
-    print('time for training classifier:', finish-start, 's')
-    print('time for training classifier per train file:', (finish-start)/len(y_train), 's')
-    f = '_clf_' + root_cute + '_' + time_stamp
-    dump(clf, open(f, 'wb'))
-    print('classifier dumped in:', f)
-    print('---')
-    return clf
-
-def test(clf, D_test, y_test):
-    print('testing...')
-    start = time()
-    y_pred = clf.predict(D_test)
-    finish = time()
-    print('time for testing:', finish-start, 's')
-    print('time for testing per test file:', (finish-start)/len(y_test), 's')
-    metrics = get_metrics(y_test, y_pred)
     f = '_metrics_' + root_cute + '_' + time_stamp
-    print(metrics, file=open(f, 'w'))
+    print(ret, file=open(f, 'w'))
     print('metrics logged in:', f)
     print('---')
-    print(metrics)
-    return metrics
+    print(ret)
+    return ret
+
+def test(clf, X, y):
+    print('testing...')
+    start = time()
+    y_pred = clf.predict(X)
+    finish = time()
+    print('time for testing:', finish-start, 's')
+    print('time for testing per file:', (finish-start)/len(y), 's')
+    get_metrics(y, y_pred)
 
 def main():
     if len(argv) < 2 or argv[1] == '-':
