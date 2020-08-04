297f148d79703f08dc63235df31b8dd2fe7adb0e
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-08-04 14:43:47 +0530

    model.py

--- Round2/model.py
+++ Round2/model.py
@@ -1,11 +1,40 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import *
+import time
 
 def get_trained_classifier(D, y):
     pipe = make_pipeline(
         DictVectorizer(),
         RandomForestClassifier()
     )
+    print('training...', end='\r')
+    start = time.time()
     pipe.fit(D, y)
+    finish = time.time()
+    print('training time:', finish-start, 's')
+    print('training time per traffic:', (finish-start)/len(y), 's')
     return pipe
+
+def predict(clf, D):
+    print('predicting...', end='\r')
+    start = time.time()
+    y_pred = clf.predict(D)
+    end = time.time()
+    print('prediction time:', finish-start, 's')
+    print('prediction time per traffic:', (finish-start)/len(D), 's')
+
+def print_metrics(y_true, y_pred):
+    print('mismatch:', ctr, '/', len(y_true))
+    print('accuracy:', accuracy_score(y_true, y_pred))
+    print('f1 (micro):', f1_score(y_true, y_pred, average='micro'))
+    print('recall (micro):', recall_score(y_true, y_pred, average='micro'))
+    print('precision (micro):', precision_score(y_true, y_pred, average='micro'))
+    print('f1 (macro):', f1_score(y_true, y_pred, average='macro'))
+    print('recall (macro):', recall_score(y_true, y_pred, average='macro'))
+    print('precision (macro):', precision_score(y_true, y_pred, average='macro'))
+
+def test(clf, D, y):
+    y_pred = predict(clf, D)
+    print_metrics(y, y_pred)
