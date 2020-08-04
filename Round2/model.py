fdfd31c7ab801f6105df5f099d9870a0bc0c8681
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-08-04 14:16:22 +0530

    minor

--- Round2/model.py
+++ Round2/model.py
@@ -2,7 +2,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import LabelEncoder
 
-def get_trained_classifier_and_label_encoder(D, y):
+def get_trained_classifier(D, y):
     pipe = make_pipeline(
         DictVectorizer(),
         RandomForestClassifier()
